9ef222ff903936a69e0807bcae456a25fb908ad4
jhrrlee <jhrrlee@gmail.com>
2024-01-27 10:44:30 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jhrrlee/go_ecom_train
+
+go 1.21.6
---
+++ routers/routers.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jhrrlee/gol_ecom_train/controllers"
+)
+
+func UserRoute(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/user/signup", controllers.Signup())
+	incomingRoutes.POST("/user/login", controllers.Login())
+	incomingRoutes.POST("/user/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/user/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/user/searcy", controllers)
+}
